0d4213c747983109faac2eabfcbafaeaf5657b51
Michel Steuwer <michel.steuwer@ed.ac.uk>
2016-09-17 17:21:26 +0100

    modifications september 2016

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-
+function toggleChevron(e) {
+    $(e.target)
+        .prev('.publication-heading')
+        .find("i.indicator")
+        .toggleClass('glyphicon-chevron-down glyphicon-chevron-right');
+}
+$('#publicationContainer').on('hidden.bs.collapse', toggleChevron);
+$('#publicationContainer').on('shown.bs.collapse', toggleChevron);
