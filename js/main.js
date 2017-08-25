63090ff34ae13ea2f22c2844fc07e7fcca97fe18
Michel Steuwer <michel.steuwer@ed.ac.uk>
2017-08-25 10:20:15 +0100

    Deleted old website, kept pdfs in case people have linked directly to them.

--- js/main.js
+++
@@ -1,8 +0,0 @@
-function toggleChevron(e) {
-    $(e.target)
-        .prev('.publication-heading')
-        .find("i.indicator")
-        .toggleClass('glyphicon-chevron-down glyphicon-chevron-right');
-}
-$('#publicationContainer').on('hidden.bs.collapse', toggleChevron);
-$('#publicationContainer').on('shown.bs.collapse', toggleChevron);
